82893eb4cfdb102c3f93fd185c63abfef3777b0c
chaliashok <chali.ashok@gmail.com>
2023-05-26 15:26:09 -0500

    Catalogue setup

--- MongoDB.sh
+++ MongoDB.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "script started"
 echo -e "\e[33mstarted\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> /tmp/robo_shop.log
 echo -e "\e[33mInstalling MongoDB\e[0m"
-yum install mongodb-org -y
-systemctl enable mongod
-systemctl start mongod
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+yum install mongodb-org -y >> /tmp/robo_shop.log
+systemctl enable mongod >> /tmp/robo_shop.log
+systemctl start mongod >> /tmp/robo_shop.log
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf >> /tmp/robo_shop.log
 systemctl restart mongod
 echo -e "\e[restart mongodb\e[0m"
---
+++ catalogue.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "script started"
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> /tmp/robo_shop.log
+
+echo "Install Node JS"
+yum install nodejs -y &>> /tmp/robo_shop.log
+
+echo "Adding Application user"
+
+useradd roboshop &>> /tmp/robo_shop.log
+
+rmdir /app
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>> /tmp/robo_shop.log
+cd /app
+unzip /tmp/catalogue.zip &>> /tmp/robo_shop.log
+
+cd /app
+npm install &>> /tmp/robo_shop.log
+
+cp /home/centos/robo-shop/catalogue.service /etc/systemd/system/catalogue.service &>> /tmp/robo_shop.log
+cp /home/centos/robo-shop/mongo.repo /etc/yum.repos.d/mongo.repo &>> /tmp/robo_shop.log
+
+systemctl daemon-reload &>> /tmp/robo_shop.log
+
+yum install mongodb-org-shell -y &>> /tmp/robo_shop.log
+
+mongo --host mongodb-dev.devopsawschinni.online </app/schema/catalogue.js &>> /tmp/robo_shop.log
+
+
+systemctl enable catalogue
+systemctl start catalogue
+
+echo "script ended"
