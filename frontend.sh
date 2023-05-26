be4c6bf7e185eb356ecf1f9828bbaf331f02a1ab
chaliashok <chali.ashok@gmail.com>
2023-05-26 08:02:36 -0500

    starting nginx server

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,7 @@
+echo -e "\e34mInstalling nginxserver\e[0m"
 yum install nginx -y
 
-
+echo -e "\e34mDeleting the old app content\e[0m"
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
