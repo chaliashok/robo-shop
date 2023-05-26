f05c26e9edc063fe166a53e66548d9e2d72d2b1e
chaliashok <chali.ashok@gmail.com>
2023-05-26 14:09:36 -0500

    MongoDB setup

--- MongoDB.sh
+++ MongoDB.sh
@@ -6,6 +6,6 @@ echo -e "\e[33mInstalling MongoDB\e[0m"
 yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
-sed -i 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl restart mongod
 echo -e "\e[restart mongodb\e[0m"
