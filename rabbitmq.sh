a0167b19d9d00e923e343456ef51fc8f98508176
chaliashok <chali.ashok@gmail.com>
2023-05-28 09:38:24 -0500

    creating rabbitmq server

---
+++ rabbitmq.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Configure YUM Repos from the script provided by vendor."
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> /tmp/robo_shop.log
+
+echo "Configure YUM Repos for RabbitMQ."
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>> /tmp/robo_shop.log
+
+echo "Installing rabbitmq"
+yum install rabbitmq-server -y &>> /tmp/robo_shop.log
+
+echo "Enabling and Starting RabbitMQ Service"
+systemctl enable rabbitmq-server
+systemctl start rabbitmq-server
+
+echo "creating one user for the application."
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+echo "Created User"
