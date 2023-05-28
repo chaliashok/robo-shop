c0dd089344a288191215ed7193b7807f2693b0fe
chaliashok <chali.ashok@gmail.com>
2023-05-28 10:43:27 -0500

    dispatch server

--- dispatch.sh
+++ dispatch.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
+log_name="/tmp/robo_shop.log"
+app_name="dispatch"
+app_dir="/app"
+
 echo "Installing golang"
-yum install golang -y &>> /tmp/robo_shop.log
+yum install golang -y &>> ${log_name}
 
 echo "Adding User"
-useradd roboshop &>> /tmp/robo_shop.log
+useradd roboshop &>> ${log_name}
 echo "Creating Directory"
-mkdir /app &>> /tmp/robo_shop.log
+mkdir ${app_dir} &>> ${log_name}
 
 echo "Downloading the depedencies"
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip &>> /tmp/robo_shop.log
-cd /app &>> /tmp/robo_shop.log
-unzip /tmp/dispatch.zip &>> /tmp/robo_shop.log
+curl -L -o /tmp/${app_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${app_name}.zip &>> ${log_name}
+cd ${app_dir} &>> ${log_name}
+unzip /tmp/${app_name}.zip &>> ${log_name}
 
 echo "Lets download the dependencies & build the software."
-go mod init dispatch &>> /tmp/robo_shop.log
-go get &>> /tmp/robo_shop.log
-go build &>> /tmp/robo_shop.log
+go mod init ${app_name} &>> ${log_name}
+go get &>> ${log_name}
+go build &>> ${log_name}
 
 echo "Copying the service file"
-cp /home/centos/robo-shop/dispatch.service /etc/systemd/system/dispatch.service &>> /tmp/robo_shop.log
+cp /home/centos/robo-shop/${app_name}.service /etc/systemd/system/${app_name}.service &>> ${log_name}
 
 echo "Reloading the service"
-systemctl daemon-reload &>> /tmp/robo_shop.log
+systemctl daemon-reload &>> ${log_name}
 
 echo "Enabling and starting the service"
-systemctl enable dispatch &>> /tmp/robo_shop.log
-systemctl start dispatch &>> /tmp/robo_shop.log
+systemctl enable ${app_name} &>> ${log_name}
+systemctl start ${app_name} &>> ${log_name}
