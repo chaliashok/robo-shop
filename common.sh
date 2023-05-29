d3802b9885b5f7a3e89e8eac6b0af3c0c15eb0e0
chaliashok <chali.ashok@gmail.com>
2023-05-29 14:45:53 -0500

    shipping items

--- common.sh
+++ common.sh
@@ -8,17 +8,26 @@ status_check(){
    else echo Failure
   fi
 }
+
+user_check(){
+id roboshop &>> ${log_name}
+status_check $?
+
+if [ "$1" -eq 1 ]; then
+  echo "success"
+else
+    useradd roboshop &>> ${log_name}
+  fi
+    }
+
 services_restart()
  {
   echo "Loading the service"
  systemctl daemon-reload &>> ${log_name}
-
  echo "enabling and starting the service"
-
  systemctl enable ${app_name} &>> ${log_name}
- systemctl start ${app_name} &>> ${log_name}
+ systemctl start  ${app_name} &>> ${log_name}
  }
-
 
 Application_setup()
  { echo "setting up directory"
@@ -26,30 +35,16 @@ Application_setup()
   mkdir ${app_dir} &>> ${log_name}
   echo "Downloading the application code to created app directory"
   curl -L -o /tmp/${app_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${app_name}.zip &>> ${log_name}
-
   echo "Downloading the dependencies"
   cd ${app_dir} &>> ${log_name}
   unzip /tmp/${app_name}.zip &>> ${log_name}
   }
 
 python (){
-
 yum install python36 gcc python3-devel -y &>> ${log_name}
 status_check $?
-
 echo "Adding user"
-user_check(){
-id roboshop &>> ${log_name}
-status_check $?
-
-if [ "$1" -eq 1 ]; then
-  echo "success"
-  else
-    useradd roboshop &>> ${log_name}
-  fi
-    }
-    user_check $?
-
+user_check $?
 Application_setup
 status_check $?
 pip3.6 install -r requirements.txt &>> ${log_name}
@@ -64,10 +59,8 @@ status_check $?
 }
 
 mysql_install() {
-
-    echo Installing mysql
+ echo Installing mysql
     yum install mysql -y &>> ${log_name}
-
     echo Loading the schema
     mysql -h mysql-dev.devopsawschinni.online -uroot -pRoboShop@1 < /${app_dir}/schema/${app_name}.sql &>> ${log_name}
   }
@@ -76,39 +69,21 @@ maven () {
 echo Installing maven
 yum install maven -y &>> ${log_name}
 status_check $?
-
 echo adding application user
-user_check(){
-id roboshop &>> ${log_name}
-status_check $?
-
-if [ "$1" -eq 1 ]; then
-  echo "success"
-else
-    useradd roboshop &>> ${log_name}
-  fi
-    }
 user_check $?
-
 Application_setup
 status_check $?
-
 echo downlaoding the dependencies
 cd ${app_dir}
 mvn clean package &>> ${log_name}
 status_check $?
 mv target/${app_name}-1.0.jar ${app_name}.jar &>> ${log_name}
 status_check $?
-
 echo copying the service file
 cp /home/centos/robo-shop/${app_name}.service /etc/systemd/system/${app_name}.service &>> ${log_name}
-
 services_restart
-
 mysql_install
-
 echo restarting shipping
 systemctl restart ${app_name} &>> ${log_name}
-
 }
 
