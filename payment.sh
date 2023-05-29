072cad44609fe70bb6dd5ec22fb65420a40463ee
chaliashok <chali.ashok@gmail.com>
2023-05-28 23:22:51 -0500

    payment

--- payment.sh
+++ payment.sh
@@ -7,6 +7,7 @@ echo "Adding user"
 useradd roboshop &>> /tmp/robo_shop.log
 
 echo "setting up directory"
+rm -rf /app
 mkdir /app &>> /tmp/robo_shop.log
 echo "Downloading the application code to created app directory"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>> /tmp/robo_shop.log
@@ -20,7 +21,7 @@ pip3.6 install -r requirements.txt &>> /tmp/robo_shop.log
 
 echo "Settingup SystemD Payment Servic"
 cp /home/centos/robo-shop/payment.service /etc/systemd/system/payment.service &>> /tmp/robo_shop.log
-sed -i -e 's/password/roboshop123/' /etc/systemd/system/payment.service &>> /tmp/robo_shop.log
+sed -i -e 's/password/$password/' /etc/systemd/system/payment.service &>> /tmp/robo_shop.log
 echo "Loading the service"
 systemctl daemon-reload &>> /tmp/robo_shop.log
 
