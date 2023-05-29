f1b0c19fa1594adb21331823fa55d035a5e646d1
chaliashok <chali.ashok@gmail.com>
2023-05-29 13:49:40 -0500

    shipping items

--- common.sh
+++ common.sh
@@ -48,6 +48,7 @@ if [ "$1" -eq 1 ]; then
   fi
     }
     user_check $?
+
 Application_setup
 status_check $?
 pip3.6 install -r requirements.txt &>> ${log_name}
@@ -60,3 +61,73 @@ status_check $?
 services_restart
 status_check $?
 }
+
+maven () {
+
+  app_directory() {
+     echo creating app directory
+    mkdir ${app_dir}
+    status_check $?
+    echo Downlaoding the application code
+    curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>> ${log_name}
+    status_check $?
+    cd ${app_dir}
+    unzip /tmp/shipping.zip &>> ${log_name}
+  }
+
+  mysql_install() {
+
+    echo Installing mysql
+    yum install mysql -y &>> ${log_name}
+
+    echo Loading the schema
+    mysql -h mysql-dev.devopsawschinni.online -uroot -pRoboShop@1 < ${app_dir}/schema/shipping.sql &>> ${log_name}
+  }
+
+echo Installing maven
+yum install maven -y &>> ${log_name}
+status_check $?
+
+echo adding application user
+
+user_check(){
+id roboshop &>> ${log_name}
+status_check $?
+
+if [ "$1" -eq 1 ]; then
+  echo "success"
+  else
+    useradd roboshop &>> ${log_name}
+  fi
+    }
+    user_check $
+
+app_directory
+status_check $?
+
+echo downlaoding the dependencies
+mvn clean package &>> ${log_name}
+status_check $?
+
+mv target/shipping-1.0.jar shipping.jar &>> ${log_name}
+status_check $?
+
+echo copying the service file
+cp /home/centos/robo-shop/shipping.service /etc/systemd/system/shipping.service &>> ${log_name}
+
+echo deamon reload
+systemctl daemon-reload &>> ${log_name}
+
+
+echo enabling and stating shipping
+
+systemctl enable shipping &>> ${log_name}
+systemctl start shipping &>> ${log_name}
+
+mysql_install
+
+echo restarting shipping
+systemctl restart shipping &>> ${log_name}
+
+}
+
--- shippind.sh
+++ shippind.sh
@@ -1,39 +1,4 @@
-echo Installing maven
-yum install maven -y &>> /tmp/robo_shop.log
+source common.sh
+maven
 
-echo adding application user
-useradd roboshop
- echo creating app directory
-mkdir /app
-echo Downlaoding the application code
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>> /tmp/robo_shop.log
-
-cd /app
-unzip /tmp/shipping.zip &>> /tmp/robo_shop.log
-
-echo downlaoding the dependencies
-mvn clean package &>> /tmp/robo_shop.log
-
-mv target/shipping-1.0.jar shipping.jar &>> /tmp/robo_shop.log
-
-echo copying the service file
-cp /home/centos/robo-shop/shipping.service /etc/systemd/system/shipping.service &>> /tmp/robo_shop.log
-
-echo deamon reload
-systemctl daemon-reload &>> /tmp/robo_shop.log
-
-
-echo enabling and stating shipping
-systemctl enable shipping &>> /tmp/robo_shop.log
-
-systemctl start shipping &>> /tmp/robo_shop.log
-
-echo Insatlling mysql
-yum install mysql -y &>> /tmp/robo_shop.log
-
-echo Loading the schema
-mysql -h mysql-dev.devopsawschinni.online -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> /tmp/robo_shop.log
-
-echo restarting shipping
-systemctl restart shipping &>> /tmp/robo_shop.log
 
