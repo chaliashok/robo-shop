c149dcfd3bec60c1797a28c3c12789319bd7b99e
chaliashok <chali.ashok@gmail.com>
2023-05-30 16:50:55 -0500

    finalising the roboshop code

--- frontend.sh
+++ frontend.sh
@@ -2,15 +2,15 @@ source common.sh
 app_name="frontend"
 
 
-echo -e "\e[33mInstalling nginxserver\e[0m"
+echo "Installing nginxserver"
 yum install nginx -y &>> ${log_name}
 status_check $?
 
-echo -e "\e[33mDeleting the old app content\e[0m"
+echo "Deleting the old app content"
 rm -rf /usr/share/nginx/html/* &>> ${log_name}
 status_check $?
 
-echo -e "\e[33mDownloading the frontend content\e[0m"
+echo "Downloading the frontend content"
 curl -o /tmp/${app_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${app_name}.zip &>> ${log_name}
 cd /usr/share/nginx/html
 unzip /tmp/${app_name}.zip &>> ${log_name}
@@ -18,7 +18,7 @@ status_check $?
 
 #need to copy config file
 cp /home/centos/robo-shop/roboshop.conf /etc/nginx/default.d/ &>> ${log_name}
-echo -e "\e[33mStarting the nginx server\e[0m"
+echo "Starting the nginx server"
 systemctl enable nginx
 systemctl restart nginx
 status_check $?
